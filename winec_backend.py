3b8d5bb9263451c8ecb81f51c7890dedff598214
fchabrun <46962341+fchabrun@users.noreply.github.com>
2025-03-12 00:02:48 +0100

    added bmp180 support

--- winec_backend.py
+++ winec_backend.py
@@ -2,20 +2,37 @@ import sqlite3
 import json
 import os
 import time
+import argparse
+import sys
+    
+root_dir = os.path.split(sys.argv[0])[0]
+sys.path.append(root_dir)
+import bmp180
 
-ROOT_DIR = "~/Documents/winec"
-DB_NAME = "winec_db_v1.db"
+# logs
+def log(s, args):
+    with open(os.path.join(args.rundir, "winec.log"), "a") as f:
+        f.write(s)
 
-# measurements
+# sqlite3 db
 def init_db():
-    connection = sqlite3.connect(DB_NAME)
+    log("intializing db", args)
+    connection = sqlite3.connect(args.db)
     cursor = connection.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS temperature_measurements (time DATETIME, left_temperature FLOAT, right_temperature FLOAT, left_tec_status BOOLEAN, right_tec_status BOOLEAN)")
     connection.commit()
     connection.close()
+    log("initialized db", args)
     
-def db_store_measurements(left_temp, right_temp, left_tec_status, right_tec_status):
-    connection = sqlite3.connect(DB_NAME)
+def clear_db(args):
+    connection = sqlite3.connect(args.db)
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS temperature_measurements")
+    connection.commit()
+    connection.close()
+    
+def db_store_measurements(left_temp, right_temp, left_tec_status, right_tec_status, args):
+    connection = sqlite3.connect(args.db)
     cursor = connection.cursor()
     cursor.execute(f"INSERT INTO temperature_measurements VALUES (DateTime('now'), {left_temp:.2f}, {right_temp:.2f}, {left_tec_status:b}, {right_tec_status:b})")
     connection.commit()
@@ -24,38 +41,47 @@ def db_store_measurements(left_temp, right_temp, left_tec_status, right_tec_stat
 # settings
 def default_params():
     params = {
-        "loop_delay_seconds": 60,
+        "loop_delay_seconds": 5,
     }
     return params
 
-def load_params():
-    json_path = os.path.join(ROOT_DIR, "settings.json")
+def load_params(args):
+    json_path = os.path.join(args.rundir, "settings.json")
     try:
         with open(json_path, "r") as f:
             params = json.load(f)
-        print(f"loaded params from json at path {json_path}")
+        log(f"loaded params from json at path {json_path}", args)
     except:
-        print(f"no params found at path {json_path}, loading defaults")
+        log(f"no params found at path {json_path}, loading defaults", args)
         params = default_params()
         try:
             with open(json_path, "w") as f:
                 json.dump(params, f)
-            print(f"saved params to json at path {json_path}")
+            log(f"saved params to json at path {json_path}"; args)
         except:
-            print(f"could not save params to json at path {json_path}")
+            log(f"could not save params to json at path {json_path}", args)
     return params
     
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--clean")
+    parser.add_argument("--rundir", default="~/Documents/winec_res")
+    parser.add_argument("--db", default="winec_db_v1.db")
+    args = parser.parse_args()
+    
+    if args.clean is not None:
+        clean_db(args)
+
     while True:
         # load params at every cycle in case something changed
-        params = load_params()
+        params = load_params(args)
         
         # get temperature measurements
         # TODO
         left_temp, right_temp = get_current_temperatures()
         
         # store new temperature measurements
-        db_store_measurements(left_temp, right_temp, left_tec_status, right_tec_status)
+        db_store_measurements(left_temp, right_temp, left_tec_status, right_tec_status, args)
         
         # decide if tec has to go on or off
         # TODO
