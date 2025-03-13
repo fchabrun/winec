df5f7d3bd96421b4daa4f1c3a635f6e7ea55e5b9
fchabrun <46962341+fchabrun@users.noreply.github.com>
2025-03-13 01:21:08 +0100

    implemented tec activation for left cav

--- winec_backend.py
+++ winec_backend.py
@@ -5,7 +5,7 @@ import time
 import argparse
 import sys
 from datetime import datetime
-# import numpy as np  # TODO remove
+import RPi.GPIO as GPIO
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode")
@@ -16,6 +16,12 @@ parser.add_argument("--port")
 parser.add_argument("--clean_db", default=True)
 parser.add_argument("--clean_params", default=True)
 parser.add_argument("--rundir", default="/home/cav/winec_rundir")
+parser.add_argument("--left_bmp180_bus", default=1)
+parser.add_argument("--left_bmp180_address", default=0x77)
+parser.add_argument("--right_bmp180_bus", default=4)
+parser.add_argument("--right_bmp180_address", default=0x77)
+parser.add_argument("--left_tec_gpio", default=22)
+parser.add_argument("--right_tec_gpio", default=23)
 # parser.add_argument("--rundir", default=r"C:\Users\flori\OneDrive - univ-angers.fr\Documents\Home\Documents\winec\rundir")
 args = parser.parse_args()
 
@@ -66,15 +72,11 @@ def default_params():
     params = {
         "loop_delay_seconds": 1,
         "left": {
-            "bmp180_bus": 1,  # bmp180 i2c bus
-            "bmp180_address": 0x77,  # bmp180 i2c address
             "target_temperature": 12.0,  # target temperature
             "temperature_deviation": 2.0,  # the algorithm will tolerate values between target - dev and target + dev before switching tec on/off
             "tec_cooldown_minutes": 5.0,  # the tec won't be activated again before waiting for the end of the cooldown delay
         },
         "right": {
-            "bmp180_bus": 4,  # bmp180 i2c bus
-            "bmp180_address": 0x77,  # bmp180 i2c address
             "target_temperature": 12.0,  # target temperature
             "temperature_deviation": 2.0,  # the algorithm will tolerate values between target - dev and target + dev before switching tec on/off
             "tec_cooldown_minutes": 5.0,  # the tec won't be activated again before waiting for the end of the cooldown delay
@@ -110,10 +112,17 @@ def get_params():
 
 # measures, etc.
 def get_current_temperatures():
+    log("getting sensor measurements")
     left_temp = left_bmp.get_temp()
     right_temp = right_bmp.get_temp()
     return left_temp, right_temp
-    # return np.random.normal(loc=12, scale=1, size=None), np.random.normal(loc=12, scale=1, size=None)
+    
+    
+def init_tec(pin):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(LedPin, GPIO.LOW)
+    
 
 if __name__ == "__main__":
     if args.clean_db is not None:
@@ -122,45 +131,40 @@ if __name__ == "__main__":
         clear_params()
 
     init_db()
-
+    
     # init actuators (tecs)
+    init_tec(args.left_tec_gpio)
+    init_tec(args.right_tec_gpio)
     left_tec_status = 0
     right_tec_status = 0
 
     # init sensors
-    left_bmp, right_bmp = None, None
+    left_bmp = bmp180(args.left_bmp180_bus, args.left_bmp180_address)
+    right_bmp = bmp180(args.right_bmp180_bus, args.right_bmp180_address)
 
     while True:
         log("loop iteration")
 
         # load params at every cycle in case something changed
         params = get_params()
-        if (left_bmp is None) or (left_bmp.bus != params["left"]["bmp180_bus"]) or (left_bmp.address != params["left"]["bmp180_address"]):
-            log(f"initializing left bmp with bus={params['left']['bmp180_bus']} and address={params['left']['bmp180_address']}")
-            left_bmp = bmp180(params["left"]["bmp180_bus"], params["left"]["bmp180_address"])
-        if (right_bmp is None) or (right_bmp.bus != params["right"]["bmp180_bus"]) or (right_bmp.address != params["right"]["bmp180_address"]):
-            log(f"initializing left right with bus={params['right']['bmp180_bus']} and address={params['right']['bmp180_address']}")
-            right_bmp = bmp180(params["right"]["bmp180_bus"], params["right"]["bmp180_address"])
 
         # get temperature measurements
-        log("getting sensor measurements")
         left_temp, right_temp = get_current_temperatures()
         
         # store new temperature measurements
-        log("storing measurements")
         db_store_measurements(left_temp, right_temp, left_tec_status, right_tec_status)
         
         # decide if tec has to go on or off
         log("measurement-based decision")
         if (left_tec_status == 1) & (left_temp < (params["left"]["target_temperature"] - params["left"]["temperature_deviation"])):
             # turn off and store
-            # TODO actually turn on
             # TODO handle cooldown
+            GPIO.output(args.left_tec_gpio, GPIO.LOW)
             left_tec_status = 0
         elif (left_tec_status == 0) & (left_temp > (params["left"]["target_temperature"] + params["left"]["temperature_deviation"])):
             # turn on and store
-            # TODO actually turn off
             # TODO handle cooldown
+            GPIO.output(args.left_tec_gpio, GPIO.HIGH)
             left_tec_status = 1
         # TODO also for right
 
