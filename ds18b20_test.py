cae3f19407cff3531dbe204446e4cff91fac92cc
fchabrun <46962341+fchabrun@users.noreply.github.com>
2025-04-12 17:35:52 +0200

    Update ds18b20_test.py

--- ds18b20_test.py
+++ ds18b20_test.py
@@ -1,29 +1,59 @@
-import glob
+import os
 import time
 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+default_w1_rootdir = '/sys/bus/w1/devices/'
 
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
 
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
+def read_temp_raw(ds18b20_addr, w1_rootdir):
+    device_filepath = os.path.join(w1_rootdir, "28-" + ds18b20_addr, "w1_slave")
+    try:
+        with open(device_filepath, 'r') as f:
+            lines = f.readlines()
+        return lines
+    except:
+        raise Exception("Unable to load w1 device file: not connected?")
+
+
+def read_temp(ds18b20_addr, w1_rootdir, timeout=1):
+    tstart = time.time()
+    while True:
+        # handle timeout
+        if time.time() - tstart > timeout:
+            raise Exception("Timeout")
+        # read device file
+        lines = read_temp_raw(ds18b20_addr=ds18b20_addr, w1_rootdir=w1_rootdir)
+        # break out of loop if file was read and is OK
+        if lines[0].strip()[-3:] == 'YES':
+            break
+        # else, wait a little bit and start again until timeout
         time.sleep(0.2)
-        lines = read_temp_raw()
+    assert lines is not None, "Unable to read device file"
+    assert len(lines) > 0, "Device file is empty"
+
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
+        temp_string = lines[1][equals_pos + 2:]
         temp_c = float(temp_string) / 1000.0
         return temp_c
+    raise Exception("Incorrect device file formatting")
+
 
 if __name__ == "__main__":
+    addr1 = "000000bb35e7"
+    addr2 = "000000bc51c5"
+
     while True:
-        temp_c = read_temp()
-        print(f"{temp_c=}")
+        # read temps
+        try:
+            temp_c1 = read_temp_raw(ds18b20_addr=addr1, w1_rootdir=default_w1_rootdir)
+            print(f"{temp_c1=}")
+        except Exception as error:
+            print(f"Unable to read temp_c1: {error=}")
+        try:
+            temp_c2 = read_temp_raw(ds18b20_addr=addr2, w1_rootdir=default_w1_rootdir)
+            print(f"{temp_c2=}")
+        except Exception as error:
+            print(f"Unable to read temp_c2: {error=}")
+        # print
+        # wait until next read
         time.sleep(1)
