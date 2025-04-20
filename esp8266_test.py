3faf6c231618840738777a7537458adb1e510ba1
fchabrun <46962341+fchabrun@users.noreply.github.com>
2025-04-20 19:35:13 +0200

    Update esp8266_test.py

--- esp8266_test.py
+++ esp8266_test.py
@@ -3,10 +3,13 @@ import time
 
 UDP_IP = "192.168.1.20"
 UDP_PORT = 4210
-MESSAGE = "13310000005"
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
+cur_temp = 9.0
 while True:
+    MESSAGE = f"{int(round(cur_temp*10)):03}10000"
+    assert len(MESSAGE) == 8, f"message is too long: {MESSAGE=}"
     sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
-    time.sleep(10);
+    cur_temp += .1;
+    time.sleep(1);
