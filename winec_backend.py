19a2f32133003615f4fac8bfcced92866f4e5e9d
fchabrun <46962341+fchabrun@users.noreply.github.com>
2025-04-30 01:07:03 +0200

    Update winec_backend.py

--- winec_backend.py
+++ winec_backend.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from gpiozero import LED
 
 # TODO in winec_display: do not refresh automatically, but on button push instead to prevent bugs
+# TODO in winec_display: have an option to display deltas instead of raw values
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode")
